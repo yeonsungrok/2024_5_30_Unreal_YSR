1dce5774a63da8123d4ceab12ac4d35bd4a18908
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-07-01 09:05:48 +0900

    7/1 sharedPtr된 api로 시작

---
+++ SGA_0701/WINAPI_Algorithm/WINAPI_Algorithm/Program/Program.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "Program.h"
+
+#include "Scene/PaintScene.h"
+#include "Scene/CollisionScene.h"
+#include "Scene/LineScene.h"
+#include "Scene/Line2Scene.h"
+
+Program::Program()
+{
+	_scene = make_shared<CollisionScene>();
+}
+
+Program::~Program()
+{
+}
+
+void Program::Update()
+{
+	_scene->Update();
+}
+
+void Program::Render(HDC hdc)
+{
+	_scene->Render(hdc);
+}
